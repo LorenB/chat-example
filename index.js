73d4c03a9acdaf0a47def55c840f527b7d78ffe1
lorenb <loren.barcus@gmail.com>
2016-02-02 16:10:53 +0000

    Implement basic user objects on (generated by the client and stored (in-memory) by the server.

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
+var users = [];
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
@@ -13,6 +15,11 @@ io.on('connection', function(socket){
   socket.on('firstMsg', function(msg) {
       var firstMsg = JSON.parse(msg);
       console.log(firstMsg);
+  });
+  
+  socket.on('userReg', function(msg) {
+      users.push(JSON.parse(msg));
+      console.log(users);
   });
 });
 
--- public/main.js
+++ public/main.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
     var socket = io();
+    var user = {};
     var isFirstMsg = true;
-    var userId = Date.now();
+    user.joinedAt = Date.now();
+    user.Id = user.joinedAt;
     var firstMsg = '';
+    socket.emit('userReg', JSON.stringify(user));
     $('form').submit(function(){
       socket.emit('chat message', $('#m').val());
-      firstMsg = JSON.stringify({"isFirstMsg": isFirstMsg, "userId": userId})
+      firstMsg = JSON.stringify({"isFirstMsg": isFirstMsg, "userId": user.Id})
       if(isFirstMsg){
         socket.emit('firstMsg', firstMsg);
       }
